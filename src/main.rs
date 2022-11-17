64450ab6ae328ce3ed0c44809e7fd41496fa8e99
Christian Hernandez <christian@chernand.io>
2022-11-16 16:46:58 -0800

    finished skeleton game
    Signed-off-by: Christian Hernandez <christian@chernand.io>

--- src/main.rs
+++ src/main.rs
@@ -83,6 +83,9 @@ fn main() -> Result<(), Box<dyn Error>> {
         if invaders.update(delta) {
             audio.play("move");
         }
+        if player.detect_hits(&mut invaders) {
+            audio.play("explode");
+        }
 
         // Draw and render
         let drawables: Vec<&dyn Drawable> = vec![&player, &invaders];
@@ -91,6 +94,16 @@ fn main() -> Result<(), Box<dyn Error>> {
         }
         let _ = render_tx.send(curr_fame);
         thread::sleep(Duration::from_millis(1));
+
+        //Win/Lose conditions
+        if invaders.all_killed() {
+            audio.play("win");
+            break 'gameloop
+        }
+        if invaders.reached_bottom() {
+            audio.play("lose");
+            break 'gameloop
+        }
     }
 
     // Cleanup
